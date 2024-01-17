33f102a42392d7ce1932bd2630047ffc2455cd6e
Thanh10101 <trinhthanh20022@gmail.com>
2024-01-17 12:53:03 +0700

    config database

--- src/app.js
+++ src/app.js
@@ -3,17 +3,44 @@ const express = require('express')
 const app = express()
 const webRouter = require('./routes/web')
 const viewEngine = require('./config/viewEngine')
+//database
+// Get the client
+const mysql = require('mysql2')
 
 //Config env
 const port = process.env.PORT || 3001
 const hostname = process.env.HOST_NAME
+const passworddb = process.env.PASSWORDDB
+
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: passworddb,
+  database: 'bookstore',
+});
+
+// A simple SELECT query
+connection.query(
+  'SELECT * FROM persons',
+  function (err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+  }
+);
+
+// Using placeholders
+
+
 
 //config view engine
 viewEngine(app)
+
 
 //router
 app.use('/', webRouter)
 
 app.listen(port, hostname, () => {
-    console.log(`http://${hostname}:${port}`)
+  console.log(`http://${hostname}:${port}`)
 })
