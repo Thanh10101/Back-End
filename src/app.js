6ef5d074b4d16328c4576727a4a9175819b39050
Thanh10101 <trinhthanh20022@gmail.com>
2024-01-30 23:14:02 +0700

    config folder

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const viewEngine = require('./config/viewEngine')
-const initWeb = require('./routes/web')
-const initApi = require('./routes/api')
+const initWeb = require('./api/v1/routes/web')
+const initApi = require('./api/v1/routes/api')
 
 //Config env
 const port = process.env.PORT || 3001
@@ -11,7 +12,11 @@ const hostname = process.env.HOST_NAME
 global.process.setMaxListeners(0)
 //config view engine
 viewEngine(app)
-
+//config cors
+app.use(cors({
+  origin: '*',
+  methods: ['POST', 'GET', 'PUT', 'DELETE']
+}))
 //specifically about POST PUT requests
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
